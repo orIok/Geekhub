73c43992ff11a1c2bbdeefdaa0632a7b55dab70f
Alexander Nemyrovsyi <waitforga@gmail.com>
2015-10-14 23:05:32 +0300

    some fixes

--- app/src/main/java/com/nemyrovskiy/o/geekhub/MainActivity.java
+++ app/src/main/java/com/nemyrovskiy/o/geekhub/MainActivity.java
@@ -4,30 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = (Button) findViewById(R.id.btn_tuskone);
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TuskOneActivity.class));
-
-            }
-        });
-
-        final Button button2 = (Button) findViewById(R.id.btn_tusktwo);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TuskTwoActivity.class));
-            }
-        });
+        findViewById(R.id.btn_tuskone).setOnClickListener(this);
+        findViewById(R.id.btn_tusktwo).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_tuskone:
+                startActivity(new Intent(MainActivity.this, TuskOneActivity.class));
+                break;
+
+            case R.id.btn_tusktwo:
+                startActivity(new Intent(MainActivity.this, TuskTwoActivity.class));
+                break;
+        }
+    }
 }
