36c17a9d489159a7496a05022de223171613383a
orIok <waitforga@gmail.com>
2015-11-03 20:00:33 +0200

    drawer added

--- app/src/main/java/com/nemyrovskiy/o/geekhub/MainActivity.java
+++ app/src/main/java/com/nemyrovskiy/o/geekhub/MainActivity.java
@@ -1,32 +1,128 @@
 package com.nemyrovskiy.o.geekhub;
 
+
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 
 // TODO: 28.10.2015 Перепілити всьо під стрінги, добавити лендскейп, добавити свої теми для кожного кольору,
 // допілить зафарбовування екшн і статусбара, удалити тоаст з 2го актівіті
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String COLOR_EXTRA = "color";
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
     public static final int REQUEST_CODE = 1;
+
+    private String[] mScreenTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        mTitle = mDrawerTitle = getTitle();
+        mScreenTitles = getResources().getStringArray(R.array.screen_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mScreenTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                getSupportActionBar().setTitle(mTitle);
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                supportInvalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
         updateColor();
 
-        findViewById(R.id.btn_tuskone).setOnClickListener(this);
-        findViewById(R.id.btn_tusktwo).setOnClickListener(this);
-        findViewById(R.id.btn_tuskthree).setOnClickListener(this);
-        findViewById(R.id.btn_tuskFour).setOnClickListener(this);
+    }
 
+
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+
+        switch (position) {
+            case 0:
+                startActivityForResult(new Intent(this, TaskOneActivity.class), REQUEST_CODE);
+
+                break;
+            case 1:
+                startActivity(new Intent(this, TaskTwoActivity.class));
+
+                break;
+            case 2:
+                startActivity(new Intent(this, TaskThreeActivity.class));
+                break;
+            case 3:
+                startActivity(new Intent(this, TaskFourActivity.class));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        if (getActionBar() != null) getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -34,6 +130,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onResume();
         updateColor();
     }
+
 
     private void updateColor() {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
@@ -48,37 +145,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             getWindow().setStatusBarColor(colorStatusBar);
 
+        mDrawerList.setBackgroundColor(colorStatusBar);
+
     }
 
-   /* @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_CODE:
-                if (data == null) return;
-                int color = data.getIntExtra(COLOR_EXTRA, android.R.color.background_light);
-                break;
-        }
-    }*/
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_tuskone:
-                startActivityForResult(new Intent(this, TaskOneActivity.class), REQUEST_CODE);
-                break;
-
-            case R.id.btn_tusktwo:
-                Intent intent = new Intent(this, TaskTwoActivity.class);
-                startActivity(intent);
-                break;
-
-            case R.id.btn_tuskthree:
-                startActivity(new Intent(this, TaskThreeActivity.class));
-                break;
-
-            case R.id.btn_tuskFour:
-                startActivity(new Intent(this, TaskFourActivity.class));
-                break;
-        }
-    }
 }
